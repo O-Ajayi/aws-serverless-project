68263252a205a77283735785326c5475c1b55cf1
o-ajayi2k1 <o.ajayi2k1@gmail.com>
2023-10-02 14:11:08 -0500

    updated cwatch config

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -122,28 +122,28 @@ resource "aws_lambda_function" "this" {
 
   tags = var.tags
 
-  # depends_on = [
-  #   null_resource.archive,
-  #   aws_s3_object.lambda_package,
+  depends_on = [
+    #   null_resource.archive,
+    #   aws_s3_object.lambda_package,
 
-  #   # Depending on the log group is necessary to allow Terraform to create the log group before AWS can.
-  #   # When a lambda function is invoked, AWS creates the log group automatically if it doesn't exist yet.
-  #   # Without the dependency, this can result in a race condition if the lambda function is invoked before
-  #   # Terraform can create the log group.
-  #   aws_cloudwatch_log_group.lambda,
+    #   # Depending on the log group is necessary to allow Terraform to create the log group before AWS can.
+    #   # When a lambda function is invoked, AWS creates the log group automatically if it doesn't exist yet.
+    #   # Without the dependency, this can result in a race condition if the lambda function is invoked before
+    #   # Terraform can create the log group.
+    aws_cloudwatch_log_group.lambda
 
-  #   # Before the lambda is created the execution role with all its policies should be ready
-  #   aws_iam_role_policy_attachment.additional_inline,
-  #   aws_iam_role_policy_attachment.additional_json,
-  #   aws_iam_role_policy_attachment.additional_jsons,
-  #   aws_iam_role_policy_attachment.additional_many,
-  #   aws_iam_role_policy_attachment.additional_one,
-  #   aws_iam_role_policy_attachment.async,
-  #   aws_iam_role_policy_attachment.logs,
-  #   aws_iam_role_policy_attachment.dead_letter,
-  #   aws_iam_role_policy_attachment.vpc,
-  #   aws_iam_role_policy_attachment.tracing,
-  # ]
+    #   # Before the lambda is created the execution role with all its policies should be ready
+    #   aws_iam_role_policy_attachment.additional_inline,
+    #   aws_iam_role_policy_attachment.additional_json,
+    #   aws_iam_role_policy_attachment.additional_jsons,
+    #   aws_iam_role_policy_attachment.additional_many,
+    #   aws_iam_role_policy_attachment.additional_one,
+    #   aws_iam_role_policy_attachment.async,
+    #   aws_iam_role_policy_attachment.logs,
+    #   aws_iam_role_policy_attachment.dead_letter,
+    #   aws_iam_role_policy_attachment.vpc,
+    #   aws_iam_role_policy_attachment.tracing,
+  ]
 }
 
 
@@ -174,12 +174,244 @@ resource "aws_lambda_function" "this" {
 #   name = "/aws/lambda/${var.lambda_at_edge ? "us-east-1." : ""}${var.function_name}"
 # }
 
-# resource "aws_cloudwatch_log_group" "lambda" {
-#   count = local.create && var.create_function && !var.create_layer && !var.use_existing_cloudwatch_log_group ? 1 : 0
+resource "aws_cloudwatch_log_group" "lambda" {
+  count = local.create && var.create_function && !var.create_layer && !var.use_existing_cloudwatch_log_group ? 1 : 0
+  # for_each = var
 
-#   name              = "/aws/lambda/${var.lambda_at_edge ? "us-east-1." : ""}${var.function_name}"
-#   retention_in_days = var.cloudwatch_logs_retention_in_days
-#   kms_key_id        = var.cloudwatch_logs_kms_key_id
+  name              = "/aws/lambda/${var.lambda_at_edge ? "us-east-1." : ""}${var.function_name}"
+  retention_in_days = var.cloudwatch_logs_retention_in_days
+  kms_key_id        = var.cloudwatch_logs_kms_key_id
 
-#   tags = merge(var.tags, var.cloudwatch_logs_tags)
+  tags = merge(var.tags, var.cloudwatch_logs_tags)
+}
+
+
+
+# resource "aws_cloudwatch_metric_alarm" "calculator-time" {
+#   alarm_name          = "hub-mft-calculator-execution-time"
+#   comparison_operator = "GreaterThanOrEqualToThreshold"
+#   evaluation_periods  = "1"
+#   metric_name         = "Duration"
+#   namespace           = "AWS/Lambda"
+#   period              = "60"
+#   statistic           = "Maximum"
+#   threshold           = var.function_name.timeout
+#   alarm_description   = "Calculator Execution Time"
+#   treat_missing_data  = "ignore"
+
+#   # insufficient_data_actions = [
+#   #   "${aws_sns_topic.alarms.arn}",
+#   # ]
+
+#   # alarm_actions = [
+#   #   "${aws_sns_topic.alarms.arn}",
+#   # ]
+
+#   # ok_actions = [
+#   #   "${aws_sns_topic.alarms.arn}",
+#   # ]
+
+#   # dimensions {
+#   #   FunctionName = var.function_name
+#   #   Resource     = var.function_name
+#   # }
+# }
+
+
+
+
+resource "aws_cloudwatch_metric_alarm" "error-count" {
+  alarm_name          = "hub-eft-${var.function_name}-error-count"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "10"
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = 1
+  alarm_description   = "Calculator Error count of lambda function"
+  treat_missing_data  = "ignore"
+
+  # insufficient_data_actions = [
+  #   "arn:aws:sns:us-east-1:416496057868:Default_CloudWatch_Alarms_Topic",
+  # ]
+
+  # alarm_actions = [
+  #   "arn:aws:sns:us-east-1:416496057868:Default_CloudWatch_Alarms_Topic",
+  # ]
+
+  # ok_actions = [
+  #   "${aws_sns_topic.alarms.arn}",
+  # ]
+
+  # dimensions {
+  #   FunctionName = var.function_name
+  #   Resource     = var.function_name
+  # }
+}
+
+resource "aws_cloudwatch_metric_alarm" "duration" {
+  alarm_name          = "hub-eft-${var.function_name}-duration"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Duration"
+  namespace           = "AWS/Lambda"
+  period              = "60"
+  statistic           = "Average"
+  threshold           = 1
+  alarm_description   = "Calculator Error count of lambda function"
+  treat_missing_data  = "ignore"
+
+  # insufficient_data_actions = [
+  #   "arn:aws:sns:us-east-1:416496057868:Default_CloudWatch_Alarms_Topic",
+  # ]
+
+  # alarm_actions = [
+  #   "arn:aws:sns:us-east-1:416496057868:Default_CloudWatch_Alarms_Topic",
+  # ]
+
+  # ok_actions = [
+  #   "${aws_sns_topic.alarms.arn}",
+  # ]
+
+  # dimensions {
+  #   FunctionName = var.function_name
+  #   Resource     = var.function_name
+  # }
+}
+
+
+
+resource "aws_cloudwatch_metric_alarm" "invocation" {
+  alarm_name          = "hub-eft-${var.function_name}-invocation"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "1"
+  metric_name         = "Invocation"
+  namespace           = "AWS/Lambda"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = 1
+  alarm_description   = "Calculator Execution Time"
+  treat_missing_data  = "ignore"
+
+  # insufficient_data_actions = [
+  #   "arn:aws:sns:us-east-1:416496057868:Default_CloudWatch_Alarms_Topic",
+  # ]
+
+  # alarm_actions = [
+  #   "arn:aws:sns:us-east-1:416496057868:Default_CloudWatch_Alarms_Topic",
+  # ]
+
+  # ok_actions = [
+  #   "${aws_sns_topic.alarms.arn}",
+  # ]
+
+  # dimensions {
+  #   FunctionName = var.function_name
+  #   Resource     = var.function_name
+  # }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##########################################################
+
+# resource "aws_cloudwatch_metric_alarm" "calculator-time" {
+#   alarm_name          = "${local.project-name}-calculator-execution-time"
+#   comparison_operator = "GreaterThanOrEqualToThreshold"
+#   evaluation_periods  = "1"
+#   metric_name         = "Duration"
+#   namespace           = "AWS/Lambda"
+#   period              = "60"
+#   statistic           = "Maximum"
+#   threshold           = aws_lambda_function.calculator.timeout * 1000 * 0.75
+#   alarm_description   = "Calculator Execution Time"
+#   treat_missing_data  = "ignore"
+
+#   insufficient_data_actions = [
+#     "${aws_sns_topic.alarms.arn}",
+#   ]
+
+#   alarm_actions = [
+#     "${aws_sns_topic.alarms.arn}",
+#   ]
+
+#   ok_actions = [
+#     "${aws_sns_topic.alarms.arn}",
+#   ]
+
+#   dimensions {
+#     FunctionName = aws_lambda_function.calculator.function_name
+#     Resource     = aws_lambda_function.calculator.function_name
+#   }
+# }
+
+
+# resource "aws_cloudwatch_metric_alarm" "this" {
+#   count = var.create_metric_alarm ? 1 : 0
+
+#   alarm_name        = var.alarm_name
+#   alarm_description = var.alarm_description
+#   actions_enabled   = var.actions_enabled
+
+#   alarm_actions             = var.alarm_actions
+#   ok_actions                = var.ok_actions
+#   insufficient_data_actions = var.insufficient_data_actions
+
+#   comparison_operator = var.comparison_operator
+#   evaluation_periods  = var.evaluation_periods
+#   threshold           = var.threshold
+#   unit                = var.unit
+
+#   datapoints_to_alarm                   = var.datapoints_to_alarm
+#   treat_missing_data                    = var.treat_missing_data
+#   evaluate_low_sample_count_percentiles = var.evaluate_low_sample_count_percentiles
+
+#   # conflicts with metric_query
+#   metric_name        = var.metric_name
+#   namespace          = var.namespace
+#   period             = var.period
+#   statistic          = var.statistic
+#   extended_statistic = var.extended_statistic
+
+#   dimensions = var.dimensions
+
+#   # conflicts with metric_name
+#   dynamic "metric_query" {
+#     for_each = var.metric_query
+#     content {
+#       id          = lookup(metric_query.value, "id")
+#       account_id  = lookup(metric_query.value, "account_id", null)
+#       label       = lookup(metric_query.value, "label", null)
+#       return_data = lookup(metric_query.value, "return_data", null)
+#       expression  = lookup(metric_query.value, "expression", null)
+#       period      = lookup(metric_query.value, "period", null)
+
+#       dynamic "metric" {
+#         for_each = lookup(metric_query.value, "metric", [])
+#         content {
+#           metric_name = lookup(metric.value, "metric_name")
+#           namespace   = lookup(metric.value, "namespace")
+#           period      = lookup(metric.value, "period")
+#           stat        = lookup(metric.value, "stat")
+#           unit        = lookup(metric.value, "unit", null)
+#           dimensions  = lookup(metric.value, "dimensions", null)
+#         }
+#       }
+#     }
+#   }
+#   threshold_metric_id = var.threshold_metric_id
+
+#   tags = var.tags
 # }
