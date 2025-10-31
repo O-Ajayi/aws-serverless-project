81c7fdaced9171b489d4c05da204fd7aafc3d499
o-ajayi2k1 <o.ajayi2k1@gmail.com>
2025-10-31 11:54:41 -0500

    updated chaos-repo

---
+++ aws-chaos-broker/Exp-BIntPrvt/terraform/lambda_infra/provider.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    dynamodb_table = "experiment_pipeline_terraform_state_demoo"
+    bucket         = "experiment-pipeline-backend-bucket-demoo"
+    region         = "us-east-1"
+    key            = "lambda_infra.terraform.tfstate"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+  default_tags {
+    tags = {
+      Team = "ResiliencyTeam",
+    }
+  }
+}
---
+++ aws-chaos-broker/Latest-Exp-BIntPrvt/terraform/lambda_infra/stepfunction.tf
@@ -0,0 +1,64 @@
+resource "aws_iam_role" "step_function_role" {
+  name = "step-function-rolee"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "states.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "step_function_policy" {
+  name = "step-function-policyy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "lambda:InvokeFunction",
+          "*"
+        ]
+        Resource = resource.aws_lambda_function.experiment_lambda.arn
+      }
+    ]
+  })
+
+  depends_on = [resource.aws_lambda_function.experiment_lambda]
+}
+
+resource "aws_iam_role_policy_attachment" "step_function_policy_attachment" {
+  policy_arn = aws_iam_policy.step_function_policy.arn
+  role       = aws_iam_role.step_function_role.name
+}
+
+resource "aws_cloudwatch_log_group" "log_group_for_sfn" {
+  name = "sfn-log-group"
+
+  tags = {
+    Environment = "production"
+    Application = "serviceA"
+  }
+
+}
+
+resource "aws_sfn_state_machine" "state_machine" {
+  name     = "Experiment_broker"
+  role_arn = aws_iam_role.step_function_role.arn
+
+  definition = templatefile("${path.module}/state_machine.json", { lambda_arn = aws_lambda_function.experiment_lambda.arn })
+
+  logging_configuration {
+    log_destination        = "${aws_cloudwatch_log_group.log_group_for_sfn.arn}:*"
+    include_execution_data = true
+    level                  = "ALL"
+  }
+}
